41c48f51875b26acdde33ac1ff708d8c8e465918
Yaroslav Zosimov <geshkocker@gmail.com>
2020-12-10 23:53:28 +0200

    added homework

---
+++ hw6_task2.py
@@ -0,0 +1,8 @@
+login = input('Enter the username: ')
+while "First" in login:
+    print(f'Welcome, {login}!')
+    break
+else:
+    print('Error!')
+
+    
---
+++ hw_t2.py
@@ -0,0 +1,8 @@
+a = int(input("Set a: "))
+b = int(input("Set b: "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
