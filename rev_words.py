91345ce7b7b0b3ff05ee3e9e36931a149c5a7a78
Yaroslav Zosimov <geshkocker@gmail.com>
2021-01-05 00:44:33 +0200

    added new hw: hw4_t1.py jennys_secret.py num_to_str.py rev_words.py

---
+++ rev_words.py
@@ -0,0 +1,13 @@
+def reverse(string):
+    words = string.split()
+    words = reversed(words)
+    return (' '.join(words))
+
+
+
+
+# string = input('Words for reverse:')
+# words = string.split()
+# words = reversed(words)
+
+# print(' '.join(words))
