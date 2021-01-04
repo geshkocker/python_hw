e9321d54c10b7633d77f748b4452bd622497368f
geshkocker <75092717+geshkocker@users.noreply.github.com>
2021-01-05 01:38:07 +0200

    Delete hw5_task1.py

--- hw5_task1.py
+++
@@ -1,8 +0,0 @@
-for x in range(1,10):
-    if x % 2 == 0:
-        print(f'{x} is even')
-    elif x % 3 == 0:
-        print(f'{x} is odd')
-    elif x % 2 != 0 or x % 3 != 0:
-        print(f'{x} in not divisible')
-        
